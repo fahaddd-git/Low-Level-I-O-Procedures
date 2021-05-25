fbdd59b8f88b888f50adaf67c3b0b7b6543c3a5f
fahad <awan.fahad@gmail.com>
2021-05-25 18:53:07 -0500

    Add project files.

---
+++ Proj6_awanf.asm
@@ -0,0 +1,30 @@
+TITLE  Low level I/O Procedure Program     (Proj6_awanf.asm)
+
+; Author: Fahad Awan
+; Last Modified: 5/25/2021
+; OSU email address:awanf@oregonstate.edu
+; Course number/section:   CS271 Section 400
+; Project Number:         6        Due Date: 6/6/2021
+; Description: 
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+ .data
+
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
