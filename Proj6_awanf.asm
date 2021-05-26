6c6a063743408703f49d960e3220838ba87a8e63
fahad <awan.fahad@gmail.com>
2021-05-26 14:34:00 -0500

    created macros

--- Proj6_awanf.asm
+++ Proj6_awanf.asm
@@ -9,17 +9,79 @@ TITLE  Low level I/O Procedure Program     (Proj6_awanf.asm)
 
 INCLUDE Irvine32.inc
 
-; (insert macro definitions here)
+; ---------------------------------------------------------------------------------
+; Name: mGetString
+;
+; Displays prompt then get's user's keyboard input into a memory location.
+;
+; Preconditions: do not use eax, ecx as arguments
+; Postconditions: EAX modified
+;
+; Receives:
+; promptOffset = prompt string offset
+; storeLocation = variable to store user input
+; lengthValue = maximum amount of characters to store
+;	
+; returns: user keyboard input stored in storeLocation
+;		   amount of bytes read in EAX
+; ---------------------------------------------------------------------------------
+mGetString MACRO promptOffset:REQ, storeLocationOffset:REQ, lengthValue:REQ
+	
 
+.code
+	PUSH	EDX
+	PUSH	ECX
+
+	MOV		EDX, promptOffset
+	CALL	WriteString
+	
+	mov edx, storeLocationOffset ; point to the buffer
+	mov ecx, lengthValue+1 ; specify max characters
+	call ReadString ; input the string
+	
+	POP		ECX
+	POP		EDX
+	
+ENDM
+
+
+; ---------------------------------------------------------------------------------
+; Name: mDisplayString
+;
+; Displays
+;
+; Preconditions: do not use eax, ecx as arguments
+; Postconditions: EAX modified
+;
+; Receives: stringOffset = offset of string to display
+;	
+;	
+; returns: user keyboard input stored in storeLocation
+;		   amount of bytes read in EAX
+; ---------------------------------------------------------------------------------
+mDisplayString MACRO stringOffset:REQ
+	PUSH	EDX
+	MOV		EDX, stringOffset
+	CALL	WriteString
+	POP		EDX
+ENDM
+
+; ...
 ; (insert constant definitions here)
 
  .data
+
+ testString	BYTE	"ABCDE",0
+ storedString BYTE	5 DUP(?)
 
 
 ; (insert variable definitions here)
 
 .code
 main PROC
+
+	mGetString			OFFSET testString,  OFFSET storedString, 5
+	mDisplayString		OFFSET	 storedString
 
 
 	Invoke ExitProcess,0	; exit to operating system
